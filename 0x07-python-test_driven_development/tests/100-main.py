e20635dc43d6ac428e2e483fd4676805cc0834d7
Gemsnille <udojamesjohn@gmail.com>
2023-04-10 11:28:36 +0000

    My Work

---
+++ 0x07-python-test_driven_development/tests/100-main.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+Matrix_mul = __import__('100-matrix_mul').matrix_mul
+
+print(matrix_mul([[1, 2], [3, 4]], [[1, 2], [3, 4]]))
+print(matrix_mul([[1, 2]], [[3, 4], [5, 6]]))
+Footer
+© 2023 GitHub, Inc.
+Footer navigation
+
+    Terms
+    Privacy
+    Security
+    Status
+    Docs
+
